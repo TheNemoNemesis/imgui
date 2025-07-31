735e900f6265e453dc0534f223dd54c7a5fe79a0
ocornut <omarcornut@gmail.com>
2025-07-31 23:39:44 +0900

    Tabs: when scrolling is enabled, track selected tabs when resizing down parent container. (#3421, #8800)

--- imgui_internal.h
+++ imgui_internal.h
@@ -2837,6 +2837,7 @@ struct IMGUI_API ImGuiTabBar
     int                 CurrFrameVisible;
     int                 PrevFrameVisible;
     ImRect              BarRect;
+    float               BarRectPrevWidth;       // Backup of previous width. When width change we enforce keep horizontal scroll on focused tab.
     float               CurrTabsContentsHeight;
     float               PrevTabsContentsHeight; // Record the height of contents submitted below the tab bar
     float               WidthAllTabs;           // Actual width of all tabs (locked during layout)
